aebede0fbe35e5f2c37711623fa6cf4d822933ef
ValeriyAnufriev <marchalmorel@gmail.com>
2020-03-18 09:29:09 +0200

    header has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test2-app';
+  navBarOpen = false;
+
+  toggleNavBar(): void {
+    this.navBarOpen = !this.navBarOpen;
+  }
 }
