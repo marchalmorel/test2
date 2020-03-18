95bebf876486bee5bbe4072048a8249abc5b2ec6
ValeriyAnufriev <marchalmorel@gmail.com>
2020-03-18 10:42:49 +0200

    added routes and auth.guard.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {NewsPageComponent} from "./news-page/news-page.component";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {ProfilePageComponent} from "./profile-page/profile-page.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'news', component: NewsPageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'profile', component: ProfilePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
